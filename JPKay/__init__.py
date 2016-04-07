4fa28f61434e8522160aed7d0832317596c47934
tku137 <tku137@gmail.com>
2016-04-07 21:04:30 +0200

    initial commit with basic project structure
    gitignore: ignore check* files (+1 squashed commits)
    
    Squashed commits:
    
    [4c36f68] fix gitignore

---
+++ JPKay/__init__.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+
+__authors__ = "Tony Fischer, Jeremy Perez"
+__copyright__ = "Copyright 2014, Tony Fischer and Jeremy Perez"
+__license__ = "MIT"
+__version__ = "0.1"
+__email__ = "tku137@gmail.com, perezgemeinschaft@googlemail.com"
+__status__ = "Development"
